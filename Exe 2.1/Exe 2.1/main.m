32de7d94b0bf7610e786c642327f9b614726b3ad
coveraller <825890700@qq.com>
2016-04-15 15:49:19 +0800

    wufan

---
+++ Exe 2.1/Exe 2.1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Exe 2.1
+//
+//  Created by franda on 16/4/15.
+//  Copyright © 2016年 franda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"promgramming is fun");
+    }
+    return 0;
+}
